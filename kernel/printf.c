c636291e238bc849a6ac9638dfd2a8e922c4febe
flyto2035 <chen2035@outlook.com>
2022-11-20 16:59:25 +0800

    lab traps Backtrace

--- kernel/printf.c
+++ kernel/printf.c
@@ -133,3 +133,13 @@ printfinit(void)
   initlock(&pr.lock, "pr");
   pr.locking = 1;
 }
+
+void
+backtrace(void)
+{
+  uint64 fp_address = r_fp();
+  while(fp_address != PGROUNDDOWN(fp_address)) {
+    printf("%p\n", *(uint64*)(fp_address-8));
+    fp_address = *(uint64*)(fp_address - 16);
+  }
+}
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -54,6 +54,8 @@ sys_sleep(void)
   int n;
   uint ticks0;
 
+  backtrace();
+
   argint(0, &n);
   if(n < 0)
     n = 0;
