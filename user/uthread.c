a965c2be0089a9fe8c827b2e873d76ade95a5008
relaxcn <chen2035@foxmail.com>
2024-04-06 22:47:50 +0800

    lab thread task 1

--- user/uthread.c
+++ user/uthread.c
@@ -10,14 +10,33 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct context {
+  uint64 ra; // return address
+  uint64 sp; // stack pointer
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+  struct context context; // register context
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
+extern void thread_switch(struct context *old, struct context *new);
               
 void 
 thread_init(void)
@@ -62,6 +81,9 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    // switch old to new thread
+    // save old thread context and restore new thread context
+    thread_switch(&t->context, &current_thread->context);
   } else
     next_thread = 0;
 }
@@ -76,6 +98,9 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  // save stack pointer to stack address
+  t->context.sp = (uint64)&t->stack[STACK_SIZE-1];
+  t->context.ra = (uint64)(*func);
 }
 
 void 
@@ -154,6 +179,7 @@ main(int argc, char *argv[])
   a_started = b_started = c_started = 0;
   a_n = b_n = c_n = 0;
   thread_init();
+  // thread_a and thread_b and thead_c are all function address
   thread_create(thread_a);
   thread_create(thread_b);
   thread_create(thread_c);
